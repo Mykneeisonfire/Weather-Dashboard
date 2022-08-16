20c746fa258cd4d9d16237daf8e38df0559e10bf
Mykneeisonfire <108244837+Mykneeisonfire@users.noreply.github.com>
2022-08-15 21:31:12 -0400

    javascript update

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,26 @@
-var ApiKey="aac1af378163b93e13d358a32d684c95"
+const apiKey="aac1af378163b93e13d358a32d684c95";
 
-function displayWeather(event){
-    event.preventDefault();
-    if(citySearch.val().trim()!==""){
-            city=citySearch.val().trim();
-            currentWeather(city);
-    }
+//Search
+
+function findCity(){
+    let city = document.querySelector("input").value;
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`).then(res => res.json()).then(data => {
+
+        localStorage.setItem('currentCity',city);
+        fetchWeather(data);
+    })
 }
+// Weather api call
+
+function fetchWeather(data) {
+    let latitude = data.coord.lat
+    let longitude = data.coord.lon
+    let city = document.querySelector("input").value;
+    fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=imperial&appid=${apiKey}`).then(res => res.json()).then(data => {
+    console.log(data)
+    weatherDisplay
+    })
+}
+
+
+function weatherDisplay
