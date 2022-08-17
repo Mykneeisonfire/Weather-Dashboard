649f7be92a13d593bcee3695dafb1d0fdc23f156
Mykneeisonfire <108244837+Mykneeisonfire@users.noreply.github.com>
2022-08-16 20:49:25 -0400

    more script

--- assets/script.js
+++ assets/script.js
@@ -17,10 +17,18 @@ function fetchWeather(data) {
     let longitude = data.coord.lon
     let city = document.querySelector("input").value;
     fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=imperial&appid=${apiKey}`).then(res => res.json()).then(data => {
-    console.log(data)
-    weatherDisplay
+    console.log(data);
+    weatherDisplay(city,data);
+    forecastDisplay(data);
     })
 }
 
 
-function weatherDisplay
+function weatherDisplay(city,data){
+    const {temp,humidity, wind_speed, uvi} = data.current;
+    const {icon, description} =data.current.weather[0]
+document.querySelector(".city").innerText = city;
+document.querySelector(.icon).src = "http://openweathermap.org/img/wn/" + icon + ".png";
+document
+
+}
